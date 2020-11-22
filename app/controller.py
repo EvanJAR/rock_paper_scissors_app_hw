4bd45b3b16e477ce59b17cbd44fa158030eecd29
Evan Reid <evan_1995@hotmail.com>
2020-11-22 23:36:59 +0000

    MVP completegit status

--- app/controller.py
+++ app/controller.py
@@ -1,12 +1,17 @@
-from flask import render_template
+from flask import render_template, request, redirect, url_for
 from app import app
 from app.models.game import Game
 from app.models.player import Player
 
-@app.route('/<player_one_choice>/<player_two_choice>')
-def play_the_game():
-    player_1 = Player("Player 1", player_one_choice)
-    player_2 = Player("Player 2", player_two_choice)
-    game = Game(player_1, player_2)
-    winner = game.get_winner()
-    return render_template('result.html', winner=winner)
+@app.route('/')
+def base():
+    return render_template('base.html')
+
+@app.route('/<choice_1>/<choice_2>')
+def play_the_game(choice_1, choice_2):
+    draw = "It's a draw"
+    player_1 = Player("Player 1", choice_1)
+    player_2 = Player("Player 2", choice_2)
+    current_game = Game(player_1, player_2)
+    winner = current_game.get_winner(player_1, player_2)
+    return render_template('result.html', choice_1=choice_1, choice_2=choice_2, winner=winner)
